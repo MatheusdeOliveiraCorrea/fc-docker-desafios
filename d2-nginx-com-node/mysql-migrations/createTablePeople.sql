ca3b5d8f9127aeb451384cf1bd140e0911fadffc
Matheus de Oliveira <matheus82pessoal@protonmail.com>
2023-12-25 22:13:21 -0300

    configurando conenection mysql

---
+++ d2-nginx-com-node/mysql-migrations/createTablePeople.sql
@@ -0,0 +1,8 @@
+Use dados; 
+
+CREATE TABLE people
+(
+  id              INT unsigned NOT NULL AUTO_INCREMENT, 
+  name            VARCHAR(150) NOT NULL,                
+  PRIMARY KEY     (id)                                  
+);
--- d2-nginx-com-node/node/index.js
+++ d2-nginx-com-node/node/index.js
@@ -1,10 +1,57 @@
 import express from "express";
-const app = express(); 
+import mysql from "mysql2";
 
+const app = express(); 
 const port = 90;
 
+const config = {
+  host     : 'banco-de-dados',
+  user     : 'root',
+  password : 'admin',
+  database : 'dados'
+};
+
+var connection = mysql.createConnection(config);
+connection.connect();
+
+var pool = mysql.createPool(config);
+
+pool.getConnection(function(err,connection){
+  if(err) {
+    console.log(err)
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+
+  connection.release();
+});
+
 app.get("/", (req, res) => {
-    res.send("<h1>Full Cycle Rocks!!!</h1>")
+    const insertQuery = `INSERT INTO people (name) VALUES ('matheus ${Date.now().toString()}')`; 
+    connection.query(insertQuery, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Erro interno do servidor');
+        }
+
+        const getQuery = `SELECT * FROM people`;
+        connection.query(getQuery, (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Erro interno do servidor');
+            }
+
+            let html = "<h1>Full Cycle Rocks!</h1>\n<h3>Tabela people: (username + datetime to string) </h3>";
+
+            for (let index = 0; index < result.length; index++) {
+                html += `\n<h5>${result[index].name}</h5>`;
+            }
+
+            res.set('Content-Type', 'text/html');
+            res.send(html);
+        });
+    });
 });
 
 app.listen({
